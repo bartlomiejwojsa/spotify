6beb88bfaac89387694ae64c901977b8bcb4648e
Bartlomiej Wojsa <bartlomiej.wojsa@antsolutions.eu>
2023-01-29 03:25:51 +0100

    feat: implement playlist view controller

---
+++ spotify/ViewModels/PlaylistHeaderViewViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlaylistHeaderCollectionReusableViewModel.swift
+//  spotify
+//
+//  Created by Bartłomiej Wojsa on 29/01/2023.
+//
+
+import Foundation
+
+struct PlaylistHeaderViewViewModel {
+    let name: String?
+    let ownerName: String?
+    let description: String?
+    let artworkURL: URL?
+}
